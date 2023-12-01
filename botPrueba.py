03a80ac277fb07e079a9dee3cb91442cc8c75d38
MauroOyhanart <mauro.oyhanart@gmail.com>
2023-12-01 12:11:08 -0300

    ultimos cambios

--- botPrueba.py
+++ botPrueba.py
@@ -8,8 +8,10 @@ import backtrader as bt
 from PIL import Image
 
 if __name__ == '__main__':
+    # Inicialización del objeto cerebro
     cerebro = bt.Cerebro()
 
+    # Configuración de los datos históricos (en este caso, datos de Yahoo Finance)
     dataDAY = bt.feeds.YahooFinanceCSVData(
         dataname='./dataset/orcl-1995-2014.txt',
         fromdate=datetime.datetime(1995, 11, 1),
@@ -17,15 +19,29 @@ if __name__ == '__main__':
         reverse=False
     )
 
+    # Adicion de los datos al cerebro
     cerebro.adddata(dataDAY)
+
+    # Adicion de la estrategia MACD al cerebro
     cerebro.addstrategy(MACDStrategy)
+
+    # Configuracion del capital inicial
     cerebro.broker.set_cash(10000.0)
+    
+    # Impresion del saldo inicial
     saldoInicial = cerebro.broker.getvalue()
     print('Saldo Inicial: %.2f' % saldoInicial)
 
+    # Ejecucion del backtest
     cerebro.run()
+
+    # Generacion y muestra del grafico
     cerebro.plot()
+
+    # Calculo y muestra del saldo final
     saldoFinal = cerebro.broker.getvalue()
     print('Saldo Final: %.2f' % saldoFinal)
+
+    # Calculo y muestra del porcentaje de ganancias
     saldoFinal = (saldoFinal * 100 / saldoInicial) - 100
     print('Porcentaje de ganancias: %.2f' % saldoFinal)
